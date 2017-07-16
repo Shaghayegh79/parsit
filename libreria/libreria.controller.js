0ce27b2427dc71dd8921b20733e42c84d016952f
shaghayegh miri <shaghayegh0908@gmail.com>
2017-07-16 14:21:06 +0200

    domenicaPom

--- libreria/libreria.controller.js
+++ libreria/libreria.controller.js
@@ -4,25 +4,36 @@
     app.controller('libreriaCtrl',['loginSvc','AutoreSvc',function(loginSvc,AutoreSvc) {
         this.doLogin = doLogin;
         this.sortBy = sortBy;
+        this.sortLibriBy = sortLibriBy;
         this.setFlag=setFlag;
 
         this.auth=false;
         this.propertyName = 'autore';
+        this.propertyLibro = 'titolo';
         this.reverse = true;
+        this.reverseLibro = true;
+
         var self = this;
 
        function sortBy(propertyName) {
             self.reverse = (self.propertyName === propertyName) ? !self.reverse : false;
             self.propertyName = propertyName;
         };
+       function sortLibriBy(propertyLibro) {
+            self.reverseLibro = (self.propertyLibro === propertyLibro) ? !self.reverseLibro : false;
+            self.propertyLibro = propertyLibro;
+        };
 
-        function setFlag(indexAutore,id){
-           self.idAutore=id;
-           if (typeof this.showFlag === 'undefined') {this.showFlag = [];}        
-           this.showFlag[indexAutore]= !this.showFlag[indexAutore];
-
+        function setFlag(indexAutore){
+           if (typeof this.showFlag === 'undefined') {
+               this.showFlag= [];
+               this.showFlag[indexAutore] = true} 
+           else{
+                var flagAttuale= this.showFlag[indexAutore];
+                this.showFlag=[];
+                this.showFlag[indexAutore]= !flagAttuale;
+           }     
         }
-
         function doLogin(){
             loginSvc.login(self.username,self.password)
                 .then(function(data){
